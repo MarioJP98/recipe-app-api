c943a82d8f99c73eae816fa5868b68142e6e3874
Mario Jimenez <mario.jimenezperez@gmail.com>
2023-10-29 12:55:42 +0100

    Validate mail & create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -11,12 +11,26 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    """Manger for users."""
+    """
+    Manger for users.
+    (using=self._db is for supporting multiple databases)
+    """
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('Users must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
