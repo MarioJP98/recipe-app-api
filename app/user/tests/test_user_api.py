e68dca7970ba6a846c20dc4489e6698e20595659
Mario Jimenez <mario.jimenezperez@gmail.com>
2023-11-12 17:30:40 +0100

    Implement Create User Api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -9,7 +9,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 
-CREATE_USER_URL=reverse('user:create')
+CREATE_USER_URL = reverse('user:create')
 
 def create_user(**params):
     """Create and return a new user."""
@@ -30,7 +30,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
-        self.assertEqual(res.status_code, status.HTTP_201.CREATED)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
